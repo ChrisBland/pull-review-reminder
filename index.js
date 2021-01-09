d85775f4edb61f98e6bef11a322bd8eee59eeda7
Chris Bland <christopher.bland@compass.com>
2021-01-09 13:05:36 -0600

    First pass at alerts

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
+const github = require('@actions/github');
+const { IncomingWebhook } = require('@slack/webhook');
 const core = require('@actions/core');
-const wait = require('./wait');
 
+const { GITHUB_TOKEN } = process.env;
 
-// most @actions toolkit packages have async methods
-async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+async function sendToSlack(url, channel, message) {
+  const webhook = new IncomingWebhook(url);
+  await webhook.send({
+    channel: channel,
+    username: 'Pull Review Reminder',
+    text: message
+  })
+}
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
+function createLookup(slackLookupKeys) {
+  let keys = slackLookupKeys.split(',');
+  let arr = keys.map((key) => {
+    let keyValuePair = key.split('|');
+    let github = keyValuePair[0];
+    let slack = keyValuePair[1];
+    return {
+      github,
+      slack
+    };
+  });
+  return function (github) {
+    let found = arr.find((a) => a.github == github);
+    if (found) {
+      return found.slack;
+    }
+    return '';
   }
+}
+
+async function run() {
+  const organization = core.getInput('organization');
+  const repo = core.getInput('repo');
+  const slackWebhook = core.getInput('slack-webhook');
+  const channel = core.getInput('channel');
+  const octokit = github.getOctokit(GITHUB_TOKEN)
+  const req = await octokit.pulls.list({
+    owner: organization,
+    repo: repo,
+    state: 'open'
+  });
+  let getSlackName = createLookup(core.getInput('slack-lookup'));
+
+  let prs = req.data;
+  let reviewsNeeds = prs.filter((pr) => pr.requested_reviewers.length);
+  let msg = reviewsNeeds.map((pr) => {
+    let msg = `${pr.title}, is waiting for your review: `
+    msg += pr.requested_reviewers.map((reviewer) => {
+      console.log('reviewer', reviewer);
+      let slack = getSlackName(reviewer.login);
+      console.log('slack', slack);
+      return `<@${slack}>`;
+    }).join(', ');
+    msg += ` ${pr.html_url} \n`
+    //
+  });
+  await sendToSlack(slackWebhook, channel, msg);
 }
 
 run();
